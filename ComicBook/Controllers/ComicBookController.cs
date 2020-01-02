a98c2163c797236c82d6c86b5af2a111d2d02595
pooya <pooyar@hotmail.com>
2020-01-02 15:59:35 +0800

    Add project files.

---
+++ ComicBook/Controllers/ComicBookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBook.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "Title";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "Sth";
+            ViewBag.Artists = new string[]
+            {
+                "Pooya",
+                "Marham"
+            };
+
+            return View();
+
+        }
+    }
+}
